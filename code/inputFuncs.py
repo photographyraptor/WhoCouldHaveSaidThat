e5325dc602d5c38d0a98b6a1b7645fd1e96ea5fe
photographyraptor <zdovahzaan@gmail.com>
2022-12-17 15:42:08 +0100

    ignorebooks

--- code/inputFuncs.py
+++ code/inputFuncs.py
@@ -1,21 +1,41 @@
 # imports
 import os
+from sklearn.feature_extraction.text import CountVectorizer
 
 # const
 bookPath = "./books"
 encodCp = "utf8"
 
-# defs
-def readTextFile(filePath):
+# defs priv
+def __readTextFile(filePath: str):
     with open(filePath, 'r', encoding = encodCp) as file:
-        lines = file.readlines()    
-    return lines
+        contents = file.read()   
+        sentences = contents.split('.') 
+    return sentences
+    
+def __wordCountLine(line: str, vectorizer):
+    vectorizer.fit(line)
+    vector = vectorizer.transform(line)
+    return vector
 
+# defs
 def readTextFiles():
     dir_list = os.listdir(bookPath)
     texts = [[]]
 
     for dir in dir_list:
-        texts.append(readTextFile(bookPath + '/' + dir))
+        texts.append(__readTextFile(bookPath + '/' + dir))
     
     return texts
+
+def wordCountLines(lines):
+    vectors = [[]]
+    vectorizer = CountVectorizer()    
+
+    
+    vectors.append(__wordCountLine(lines[1], vectorizer))
+
+    #for line in lines:
+     #   vectors.append(__wordCountLine(line, vectorizer))
+
+    print(vectorizer.vocabulary_)
